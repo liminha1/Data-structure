cd263dcec1d36ad1c6efc2aca40cff924ec194cb
liminha <inhag1131@gmail.com>
2023-02-06 10:52:59 +0900

    0302EX

--- 0206_1.py
+++ 0206_1.py
@@ -1,14 +1,30 @@
-katok = []
+katok = ["다현", "정연", "쯔위", "사나", "지효"]
 
-def add_data(friend) :
+
+def insert_data(position, friend) :
+
+    if position < 0 or position > len(katok) :
+        print("데이터를 삽입할 범위를 벗어났습니다.")
+        return
 
     katok.append(None)
-    katok[len(katok)-1] = friend
 
-add_data('다현')
-add_data('정연')
-add_data('쯔위')
-add_data('사나')
-add_data('지효')
+    for i in range(len(katok) - 1, position, -1):
+        katok[i] = katok[i-1]
+        katok[i-1] = None
+
+    katok[position] = friend
 
 print(katok)
+insert_data(2, '솔라')
+print(katok)
+insert_data(6, '문별')
+print(katok)
+
+if __name__ == "__main__":
+	pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+	print(pokemons)
+	pokemons.insert(3, '어니부기' )
+	print(pokemons)
+	pokemons.insert(6, '거북왕' )
+	print(pokemons)
