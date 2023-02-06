fee517ecfa30f2272c63ffcd3eb7a2a440666152
liminha <inhag1131@gmail.com>
2023-02-06 11:44:04 +0900

    SELF_ST_0301

--- 0206_1.py
+++ 0206_1.py
@@ -7,20 +7,22 @@ def delete_data(position):
         print("데이터를 삭제할 범위를 벗어났습니다.")
         return
 
-    katok[position] = None
+    for i in range(position, len(katok)):  # 10, 11 없어도 작동됨
+        katok[i] = None   # i 부터 끝까지 None 처리
 
-    for i in range(position + 1, len(katok)) :
-        katok[i-1] = katok[i]
-        katok[i] = None
+    for _ in range(len(katok), position, -1) :
+        # katok[i-1] = katok[i]
+        # katok[i] = None
+        katok.pop()   # 끝부터 i까지 리스트 삭제
 
     # del(katok[len(katok) - 1])
-    katok.pop()
+    # katok.pop()
 
 print(katok)
 delete_data(1)
 print(katok)
-delete_data(3)
-print(katok)
+# delete_data(3)
+# print(katok)
 
 if __name__ == "__main__":
     katok = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
