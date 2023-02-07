3551073712d3e1c0b95cb03dc53d46bb48394515
liminha <inhag1131@gmail.com>
2023-02-07 15:37:19 +0900

    0802EX

--- 0207/0207.py
+++ 0207/0207.py
@@ -27,8 +27,36 @@ node6 = TreeNode()
 node6.data = '선미'
 node3.left = node6
 
-print(node1.data, end=' ')
-print()
-print(node1.left.data, node1.right.data, end=' ')
-print()
-print(node1.left.left.data, node1.left.right.data, node1.right.left.data, end=' ')
+def preorder(node) :
+    if node == None:
+        return
+    print(node.data, end= ' -> ')
+    preorder(node.left)
+    preorder(node.right)
+
+def inorder(node):
+    if node == None :
+        return
+    inorder(node.left)
+    print(node.data, end = ' -> ')
+    inorder(node.right)
+
+def postorder(node):
+    if node == None :
+        return
+    postorder(node.left)
+    postorder(node.right)
+    print(node.data, end = ' -> ')
+
+print('전위 순회 : ', end= ' ')
+preorder(node1)
+print('끝')
+
+print('중위 순회 : ', end = ' ')
+inorder(node1)
+print('끝')
+
+print('후위 순회 : ', end = ' ')
+postorder(node1)
+print('끝')
+
