1b0793b8878e039aeb13f37fdc26026528019609
liminha <inhag1131@gmail.com>
2023-02-08 10:31:58 +0900

    SELF_ST_0901

--- 0208/0208.py
+++ 0208/0208.py
@@ -4,13 +4,14 @@ class Graph() :
         self.SIZE = size
         self.graph = [ [0 for _ in range(size)] for _ in range(size)]
 
-G1, G3 = None, None
+# G1, G3 = None, None
+G1 = None
 
 G1 = Graph(4)
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
+G1.graph[0][3] = 1
+G1.graph[1][2] = 1; G1.graph[1][3] = 1
+G1.graph[2][1] = 1
+G1.graph[3][0] = 1; G1.graph[3][1] = 1
 
 print('## G1 무방향 그래프 ##')
 for row in range(4) :
@@ -18,13 +19,13 @@ for row in range(4) :
         print(G1.graph[row][col], end = ' ')
     print()
 
-G3 = Graph(4)
-G3.graph[0][1] = 1; G3.graph[0][2] = 1
-G3.graph[3][0] = 1; G3.graph[3][2] = 1
-
-print('## G3 방향 그래프 ##')
-for row in range(4):
-    for col in range(4):
-        print(G3.graph[row][col], end=' ')
-    print()
+# G3 = Graph(4)
+# G3.graph[0][1] = 1; G3.graph[0][2] = 1
+# G3.graph[3][0] = 1; G3.graph[3][2] = 1
+#
+# print('## G3 방향 그래프 ##')
+# for row in range(4):
+#     for col in range(4):
+#         print(G3.graph[row][col], end=' ')
+#     print()
 
