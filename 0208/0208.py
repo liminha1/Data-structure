93bd3091d28545191682e2ef255b759a50005bc0
liminha <inhag1131@gmail.com>
2023-02-08 16:09:19 +0900

    0402EX

--- 0208/0208.py
+++ 0208/0208.py
@@ -23,9 +23,8 @@ node5 = Node()
 node5.data = "지지"
 node4.link = node5
 
-print(node1.data, end = ' ')
-print(node1.link.data, end=' ')
-print(node1.link.link.data, end = ' ')
-print(node1.link.link.link.data, end = ' ')
-print(node1.link.link.link.link.data, end = ' ')
-
+current = node1
+print(current.data, end=' ')
+while current.link != None :
+    current = current.link
+    print(current.data, end=' ')
