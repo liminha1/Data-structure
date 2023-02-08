ce133b48db8769706d1e552415c73d163ba23a74
liminha <inhag1131@gmail.com>
2023-02-08 11:23:48 +0900

    0903EX

--- 0208/0208.py
+++ 0208/0208.py
@@ -4,32 +4,42 @@ class Graph() :
         self.SIZE = size
         self.graph = [ [0 for _ in range(size)] for _ in range(size)]
 
-def printGraph(g) :
-    print(' ', end=' ')
-    for v in range(g.SIZE) :
-        print(nameAry[v], end=' ')
-    print()
-    for row in range(g.SIZE) :
-        print(nameAry[row], end=' ')
-        for col in range(g.SIZE) :
-            print(g.graph[row][col], end=' ')
-        print()
-    print()
-
 G1 = None
-nameAry = ['문문', '솔솔', '휘휘', '쯔쯔', '선선', '화화']
-문문, 솔솔, 휘휘, 쯔쯔, 선선, 화화 = 0, 1, 2, 3, 4, 5
+stack = []
+visitedAry = []
 
-gSize = 6
-G1 = Graph(gSize)
-G1.graph[문문][솔솔] = 1; G1.graph[문문][휘휘] = 1
-G1.graph[솔솔][문문] = 1; G1.graph[솔솔][쯔쯔] = 1
-G1.graph[휘휘][문문] = 1; G1.graph[휘휘][쯔쯔] = 1
-G1.graph[쯔쯔][솔솔] = 1; G1.graph[쯔쯔][휘휘] = 1; G1.graph[쯔쯔][선선] = 1
-G1.graph[선선][쯔쯔] = 1; G1.graph[선선][화화] = 1
-G1.graph[화화][쯔쯔] = 1; G1.graph[화화][선선] = 1
+G1 = Graph(4)
+G1.graph[0][2] = 1; G1.graph[0][3] = 1
+G1.graph[1][2] = 1
+G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
+G1.graph[3][0] = 1; G1.graph[3][2] = 1
 
 print('## G1 무방향 그래프 ##')
-printGraph(G1 )
+for row in range(4) :
+    for col in range(4):
+        print(G1.graph[row][col], end= ' ')
+    print()
 
+current = 0
+stack.append(current)
+visitedAry.append(current)
 
+while (len(stack) != 0):
+    next = None
+    for vertex in range(4):
+        if G1.graph[current][vertex] == 1:
+            if vertex in visitedAry :
+                pass
+            else :
+                next = vertex
+                break
+    if next != None:
+        current = next
+        stack.append(current)
+        visitedAry.append(current)
+    else:
+        current = stack.pop()
+
+print('방문 순서 -->', end=' ')
+for i in visitedAry:
+    print(chr(ord('1')+i), end='  ')
