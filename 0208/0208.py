1952475bad3f46b18dc026a2c985b05667f5df37
liminha <inhag1131@gmail.com>
2023-02-08 16:49:37 +0900

    0405EX

--- 0208/0208.py
+++ 0208/0208.py
@@ -1,33 +1,35 @@
 class Node() :
-
     def __init__ (self) :
         self.data = None
         self.link = None
 
-node1 = Node()
-node1.data = "다다"
 
-node2 = Node()
-node2.data = "정정"
-node1.link = node2
+def printNodes(start) :
+    current = start
+    if current == None:
+        return
+    print(current.data, end = ' ')
+    while current.link != None:
+        current = current.link
+        print(current.data, end=' ')
+    print()
 
-node3 = Node()
-node3.data = "쯔쯔"
-node2.link = node3
+memory = []
+head, current, pre = None, None, None
+dataArray = ["다다", "정정", "쯔쯔", "사사", "지지"]
 
-node4 = Node()
-node4.data = "사사"
-node3.link = node4
+if __name__ == "__main__":
 
-node5 = Node()
-node5.data = "지지"
-node4.link = node5
+    node = Node()
+    node.data = dataArray[0]
+    head = node
+    memory.append(node)
 
-node2.link = node3.link
-del(node3)
+    for data in dataArray[1:] :
+        pre = node
+        node = Node()
+        node.data = data
+        pre.link = node
+        memory.append(node)
 
-current = node1
-print(current.data, end=' ')
-while current.link != None :
-    current = current.link
-    print(current.data, end=' ')
+    printNodes(head)
