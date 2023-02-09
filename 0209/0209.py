8c920829d1a4e373c34bf46dc58aa2ba2cb71e0c
liminha <inhag1131@gmail.com>
2023-02-09 14:50:05 +0900

    0708EX

--- 0209/0209.py
+++ 0209/0209.py
@@ -38,12 +38,30 @@ def peek() :
 	return queue[front + 1]
 
 
-SIZE = 5
-queue = [ "화화", "솔솔", "문문", None, None]
+SIZE = int(input("큐의 크기를 입력하세요 ==> "))
+queue = [ None for _ in range(SIZE) ]
 front = -1
-rear = 2
+rear = -1
 
-print(queue)
-retData = peek()
-print("다음에 추출될 데이터 확인 -->", retData)
-print(queue)
+if __name__ == "__main__" :
+	select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+
+	while select != 'X' and select !='x' :
+		if select == 'I' or select =='i' :
+			data = input("입력할 데이터 ==> ")
+			enQueue(data)
+			print("큐 상태: ", queue)
+		elif select == 'E' or select == 'e' :
+			data = deQueue()
+			print("추출된 데이터 ==> ", data)
+			print("큐 상태: ", queue)
+		elif select=='V' or select =='v' :
+			data = peek()
+			print("확인된 데이터 ==> ", data)
+			print("큐 상태 : ", queue)
+		else :
+			print("입력이 잘못됨")
+
+		select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+
+	print("프로그램 종료!")
