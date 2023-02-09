bfb6a9159aadfd28a55c7eaf6fcddfde4d63de28
liminha <inhag1131@gmail.com>
2023-02-09 14:29:20 +0900

    0704EX

--- 0209/0209.py
+++ 0209/0209.py
@@ -1,13 +1,24 @@
 def isQueueFull() :
 	global SIZE, queue, front, rear
-	if reat == SIZE-1 :
+	if rear == SIZE-1 :
 		return True
 	else :
 		return False
 
-SIZE = 5
-queue = ["화화", "솔솔", "문문", "휘휘", "선선"]
-front = -1
-reat = 4
+def enQueue(data) :
+	global SIZE, queue, front, rear
+	if isQueueFull() :
+		print("큐가 꽉 찼습니다.")
+		return
+	rear += 1
+	queue[rear] = data
 
-print("큐가 꽉 찼는지 여부 ==>", isQueueFull())
+SIZE = 5
+queue = ["화화", "솔솔", "문문", "휘휘", None]
+front = -1
+rear = 3
+
+print(queue)
+enQueue("선선")
+print(queue)
+enQueue("재재")
