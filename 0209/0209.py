bc96aa2d6b42e18d62efd05f0656b08a4c041a19
liminha <inhag1131@gmail.com>
2023-02-09 14:15:26 +0900

    0701EX

--- 0209/0209.py
+++ 0209/0209.py
@@ -1,73 +1,17 @@
-def isStackFull() :
-	global SIZE, stack, top
-	if (top >= SIZE-1) :
-		return True
-	else :
-		return False
+queue = [None, None, None, None, None]
+front = rear = -1
 
-def isStackEmpty() :
-	global SIZE, stack, top
-	if (top == -1) :
-		return True
-	else :
-		return False
+rear += 1
+queue[rear] = "화화"
 
-def push(data) :
-	global SIZE, stack, top
-	if (isStackFull()) :
-		print("스택이 꽉 찼습니다.")
-		return
-	top += 1
-	stack[top] = data
+rear += 1
+queue[rear] = "솔솔"
 
-def pop() :
-	global SIZE, stack, top
-	if (isStackEmpty()) :
-		print("스택이 비었습니다.")
-		return None
-	data = stack[top]
-	stack[top] = None
-	top -= 1
-	return data
+rear += 1
+queue[rear] = "문문"
 
-def peek() :
-	global SIZE, stack, top
-	if (isStackEmpty()) :
-		print("스택이 비었습니다.")
-		return None
-	return stack[top]
-
-def checkBracket(expr) :
-	for ch in expr:
-		if ch in '([{<':
-			push(ch)
-		elif ch in ')]}>' :
-			out = pop()
-			if ch == ')' and out == '(':
-				pass
-			elif ch == ']' and out == '[':
-				pass
-			elif ch == '}' and out == '{':
-				pass
-			elif ch == '>' and out == '<':
-				pass
-			else:
-				return False
-		else :
-			pass
-	if isStackEmpty() :
-		return True
-	else:
-		return False
-
-SIZE = 100
-stack = [ None for _ in range(SIZE)]
-top = -1
-
-if __name__ == "__main__":
-
-	exprAry = ['(A+B)', ')A+B(', '((A+B)-C', '(A+B]', '[(<A+{B-C}/[C*D]>)]']
-
-	for expr in exprAry:
-		top = -1
-		print(expr, '==>', checkBracket(expr))
+print("----- 큐 상태 -----")
+print('[출구] <-- ', end = '')
+for i in range(0, len(queue), 1) :
+	print(queue[i], end=' ')
+print( ' <-- [입구] ')
